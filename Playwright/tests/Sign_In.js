b988c718afd3064970ad35f588a971ff779f56aa
Vedrana Dobric <veddob1@gmail.com>
2024-04-18 19:33:26 +0200

    Sign In tests added

--- Playwright/tests/Sign_In.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("https://magento.softwaretestingboard.com/");
-  await page.getByLabel("Consent", { exact: true }).click();
-  await page.getByRole("link", { name: "Sign In" }).click();
-  await page.getByRole("button", { name: "Sign In" }).click();
-});
