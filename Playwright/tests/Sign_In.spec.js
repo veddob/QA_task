b96242cd7e8e3f25e583ea6af8dadcd3d9535e5a
Vedrana Dobric <veddob1@gmail.com>
2024-04-18 19:38:22 +0200

    Update Sign_In.spec.js

--- Playwright/tests/Sign_In.spec.js
+++ Playwright/tests/Sign_In.spec.js
@@ -30,3 +30,31 @@ test("test3", async ({ page }) => {
   await page.getByLabel("Password").fill("randompass");
   await page.getByRole("button", { name: "Sign In" }).click();
 });
+
+test("test4", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  await page.getByLabel("Consent", { exact: true }).click();
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await page.getByRole("link", { name: "Forgot Your Password?" }).click();
+  await page.getByRole("button", { name: "Reset My Password" }).click();
+});
+
+test("test4", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  await page.getByLabel("Consent", { exact: true }).click();
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await page.getByRole("link", { name: "Forgot Your Password?" }).click();
+  await page.getByLabel("Email", { exact: true }).click();
+  await page.getByLabel("Email", { exact: true }).fill("abc@");
+  await page.getByRole("button", { name: "Reset My Password" }).click();
+});
+
+test("test4", async ({ page }) => {
+  await page.goto("https://magento.softwaretestingboard.com/");
+  await page.getByLabel("Consent", { exact: true }).click();
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await page.getByRole("link", { name: "Forgot Your Password?" }).click();
+  await page.getByLabel("Email", { exact: true }).click();
+  await page.getByLabel("Email", { exact: true }).fill("abc@gmail.com");
+  await page.getByRole("button", { name: "Reset My Password" }).click();
+});
